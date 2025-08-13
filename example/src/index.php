961fa5a1edb25a83b75a3488e70cbcf2fd192687
Matheus Augusto <auguzsto@gmail.com>
2025-08-13 11:05:25 -0300

    fix: example

--- example/src/index.php
+++ example/src/index.php
@@ -1,9 +1,10 @@
 <?php
 
+use Auguzsto\Example\Tasks\Backup;
 use Auguzsto\Job\Job;
 
     require_once __DIR__ . "/../vendor/autoload.php";
     
     $job = new Job();
-    $result = $job->execute("Auguzsto\Example\Tasks\Backup::large");
+    $result = $job->execute(Backup::class, "large");
     print($job->pid);
