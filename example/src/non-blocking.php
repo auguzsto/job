74688e945d5368c4d3d25b319e1ab32ef961597f
Matheus Augusto <auguzsto@gmail.com>
2025-08-28 18:05:29 -0300

    docs: example

--- example/src/non-blocking.php
+++ example/src/non-blocking.php
@@ -5,6 +5,6 @@ use Auguzsto\Job\Job;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-$job = new Job(LargerBackup::class, "go", [60]);
+$job = new Job(LargerBackup::class, "go", [3]);
 $worker = $job->execute();
 print "Task sent to worker $worker" . PHP_EOL;
