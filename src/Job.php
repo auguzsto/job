4839b4beb504eadf916116ade7b3e9c72e83a65b
Matheus Augusto <auguzsto@gmail.com>
2025-08-28 23:54:16 -0300

    fix: building array once in job->execute()

--- src/Job.php
+++ src/Job.php
@@ -21,7 +21,10 @@ class Job implements JobInterface
     public function execute(): int
     {
         try {
-            if (!$this->checkWorkersEnables()) {
+            $dirworkers = Worker::DIR;
+            $workers = array_diff(scandir($dirworkers), [".", ".."]);
+
+            if (!$this->checkWorkersEnables($workers)) {
                 throw new NoActiveWorkersException("No active workers. Try restarting.");
             }
 
@@ -32,14 +35,12 @@ class Job implements JobInterface
             if (!$this->checkMethodExists($this->class, $this->method)) {
                 throw new MethodNotExistsException("Method not found");
             }
-            $dirworkers = Worker::DIR;
 
-            $workers = array_diff(scandir($dirworkers), [".", ".."]);
             $randomId = random_int(1, count($workers));
             $fileWorker = "$dirworkers/$randomId";
             $content = file_get_contents($fileWorker);
             $worker = unserialize($content);
-            
+
             if (empty($worker["callable"])) {
                 $worker["callable"] = [$this->class, $this->method, $this->args];
                 file_put_contents($fileWorker, serialize($worker));
@@ -53,14 +54,12 @@ class Job implements JobInterface
         }
     }
 
-    private function checkWorkersEnables(): bool
+    private function checkWorkersEnables(array $workers): bool
     {
-        $dir = Worker::DIR;
-        $workers = array_diff(scandir($dir), [".", ".."]);
         if (count($workers) > 0) {
             return true;
         }
-        
+
         return false;
     }
 
